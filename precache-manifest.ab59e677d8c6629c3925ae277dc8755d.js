34f960b7acf25b5648b1cbbc4f68f64a686c2291
kfuseini <kf1112@nyu.edu>
2019-12-29 16:28:52 -0500

    Updates

---
+++ precache-manifest.ab59e677d8c6629c3925ae277dc8755d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38022d8b0c08b28e36d5aaec02cbbe41",
+    "url": "/bare-react-app/index.html"
+  },
+  {
+    "revision": "e03929da2414f77efc1c",
+    "url": "/bare-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b88751efbf5e6ac56f2c",
+    "url": "/bare-react-app/static/js/2.6fff198d.chunk.js"
+  },
+  {
+    "revision": "ce028d5e2b0507b0fb507336501c1edf",
+    "url": "/bare-react-app/static/js/2.6fff198d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e03929da2414f77efc1c",
+    "url": "/bare-react-app/static/js/main.67afdbe7.chunk.js"
+  },
+  {
+    "revision": "caa5c51fa154412dacf3",
+    "url": "/bare-react-app/static/js/runtime-main.5e21a2df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bare-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
